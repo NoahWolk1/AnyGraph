0ca7eb0d7fec228ca0246369f1e82750dafab90a
Noah Wolk <nwolk23@Noah-Wolk-FVFHK008Q6L6-10004857.local>
2023-09-18 10:59:56 -0500

    Initial Commit

---
+++ chatgptData.py
@@ -0,0 +1,35 @@
+'''import openai
+
+openai.api_key = "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"
+
+def chat_with_chatgpt(prompt, model="gpt-3.5-turbo"):
+    response = openai.Completion.create(
+        engine=model,
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = response.choices[0].text.strip()
+    return message
+
+user_prompt = "Write a summary of the benefits of exercise."
+chatbot_response = chat_with_chatgpt(user_prompt)
+print(chatbot_response)'''
+
+import openai
+import os
+
+openai.api_key = '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'
+
+completion = openai.ChatCompletion.create(
+  model = 'gpt-3.5-turbo',
+  messages = [
+    {'role': 'assistant', 'content': 'provide data for number of nuclear bombs per year. provide data in the form of (x,y) that is it, no other words other than titles of data.'}
+  ],
+  temperature = 0  
+)
+
+print(completion['choices'][0]['message']['content'])
